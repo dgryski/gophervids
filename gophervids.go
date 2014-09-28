482265b71541ce1715de49d2d6683e1c4467e5dc
Damian Gryski <damian@gryski.com>
2014-09-28 17:17:40 +0200

    Add RSS and Atom feeds
    Fixes #11.

--- gophervids.go
+++ gophervids.go
@@ -1,5 +1,58 @@
 package gophervids
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/feeds"
+)
+
+type vid struct {
+	Date     string
+	Added    string
+	Id       string
+	Tags     []string
+	Speakers []string
+	Title    string
+}
+
 func init() {
-	// no handlers registered, just a static site for now
+
+	js, _ := ioutil.ReadFile("static/vids.json")
+
+	var vids []vid
+
+	json.Unmarshal(js, &vids)
+
+	feed := &feeds.Feed{
+		Title:       "gophervids",
+		Link:        &feeds.Link{Href: "http://gophervids.appspot.com"},
+		Description: "categorized golang videos",
+		Author:      &feeds.Author{"Damian Gryski", "damian@gryski.com"},
+		Created:     time.Now(),
+	}
+
+	for _, v := range vids {
+		updated, _ := time.Parse("2006-01-02", v.Date)
+		var speaker string
+		if len(v.Speakers) > 0 {
+			speaker = v.Speakers[0]
+		}
+		feed.Items = append(feed.Items, &feeds.Item{
+			Title:   v.Title,
+			Link:    &feeds.Link{Href: "https://youtu.be/" + v.Id},
+			Author:  &feeds.Author{speaker, ""},
+			Created: updated,
+		})
+	}
+
+	http.HandleFunc("/rss.xml", func(w http.ResponseWriter, r *http.Request) {
+		feed.WriteRss(w)
+	})
+
+	http.HandleFunc("/atom.xml", func(w http.ResponseWriter, r *http.Request) {
+		feed.WriteAtom(w)
+	})
 }
